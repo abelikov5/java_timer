3077958e73e2661685fa998dec0d2f9309349539
Alexander <abelikov@inbox.ru>
2021-09-16 15:12:37 +0300

    timmer app designer

---
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void TimerStart(View view) {
+
+    }
+
+    public void TimerPause(View view) {
+    }
+
+    public void TimerReset(View view) {
+    }
+}
